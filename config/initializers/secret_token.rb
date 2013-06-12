85e1d8a7960ee8039d3417b7886166c03d7e45b3
zkellerdevelops <zkellerdevelops@gmail.com>
2013-06-12 11:27:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapstoneDeadsets::Application.config.secret_token = 'd472b295c11979328836a95c82d89a1c6df5cfc272c10c973b71676cd770e0fbb25ae914387975b7d0d20249ea7f5a8b57efec2d827a9f861d154e84aea119b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CapstoneDeadsets::Application.config.session_store :cookie_store, key: '_capstone-deadsets_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CapstoneDeadsets::Application.config.session_store :active_record_store
