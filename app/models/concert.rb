eeab894bc93d133882313cbd126ef7dfd5147acb
zkellerdevelops <zkellerdevelops@gmail.com>
2013-06-19 16:03:37 -0500

    slice zero on all characters implemented

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,9 +3,13 @@ class Concert < ActiveRecord::Base
 
   def self.search(search)
     if search
-      if search.starts_with? "0"
-        search.slice!(0)
-      end
+      search_split = search.split("-")
+        search_split.each do |search|
+          if search.starts_with? "0"
+            search.slice!(0)
+          end
+        end
+      search = search_split.join("-").to_s
       where(:date => search).all
     else
       []
